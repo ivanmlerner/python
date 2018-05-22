01a5cc472b8c391eef970395cd5f7d3b505500d7
Ivan Marquesi Lerner <ivanml1992@gmail.com>
2018-05-22 17:07:29 -0300

    Delete E3L2.py

--- L2/E3L2.py
+++
@@ -1,8 +0,0 @@
-excesso = multa = 0
-peso = float(input("Peso total de peixes (kg): "))
-
-if peso > 50:
-    excesso = peso - 50
-    multa = excesso * 4
-
-print("Excedeu o limite em " + str(excesso) + "kg\nMulta: R$" + str(multa))
