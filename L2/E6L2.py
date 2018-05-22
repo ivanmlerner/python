c88744aa17cb98d82c119be11aaed9d3b86cf4f8
Ivan Marquesi Lerner <ivanml1992@gmail.com>
2018-05-22 17:07:50 -0300

    Delete E6L2.py

--- L2/E6L2.py
+++
@@ -1,7 +0,0 @@
-sal_tot = float(input("Quanto você ganha por hora? ")) * float(input("Quantas horas trabalhou no mês? "))
-des = "Desconto para o"
-
-print(des, "IR: R$" + str(0.11 * sal_tot))
-print(des, "INSS: R$" + str(0.08 * sal_tot))
-print(des, "sindicato: R$" + str(0.05 * sal_tot))
-print("Salário líquido: R$" + str(0.24 * sal_tot))
