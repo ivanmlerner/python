2b62441b3720209fefb51ee91203f1fc2b0655a3
Ivan Marquesi Lerner <ivanml1992@gmail.com>
2018-05-22 17:07:44 -0300

    Delete E5L2.py

--- L2/E5L2.py
+++
@@ -1,6 +0,0 @@
-num = []
-
-for n in range(int(input("Achar os extremos entre quantos números? "))):
-    num.append(float(input("Digite o " + str(n + 1) + "º número: ")))
-
-print("Maior:", max(num), "\nMenor:", min(num)) 
